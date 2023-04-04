cce0b750881c171cd0c57dfdd00b86a20bfe28f1
Levente Krotos <krotos.levi@gmail.com>
2023-04-04 19:45:54 +0200

    very basic getAll method to proceed with FE part of phone-webshop

---
+++ main.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import express from 'express';
+import AWS from 'aws-sdk';
+
+const app = express();
+const s3 = new AWS.S3();
+
+app.getAll('/data', (req, res) => {
+  const params = {
+    Bucket: 'phone-webshop',
+    Key: 'api/phones.json',
+  };
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      const jsonFile = JSON.parse(data.Body.toString());
+      res.send(jsonFile);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
