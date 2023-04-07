fb4a25e04069847518da3ea0864c6605e35049bc
Levente Krotos <krotos.levi@gmail.com>
2023-04-07 15:09:08 +0200

    added exact phone request

--- main.js
+++ main.js
@@ -24,6 +24,23 @@ app.get('/data', (req, res) => {
   });
 });
 
+app.get('/data/:phoneId', (req, res) => {
+  const { phoneId } = req.params;
+  const params = {
+    Bucket: 'phone-webshop',
+    Key: `api/phones/${phoneId}.json`,
+  };
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      const jsonFile = JSON.parse(data.Body.toString());
+      res.send(jsonFile);
+    }
+  });
+});
+
 app.listen(3001, () => {
   console.log('Server listening on port 3000');
 });
